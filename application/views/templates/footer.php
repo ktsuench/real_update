546540f9ab191ec572dc748980ad1d7c6c8cea09
ktsuench <will289@hotmail.com>
2016-06-04 23:59:35 -0400

    added in do not display option for copyright in footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,7 @@
         <div>
+        	<?php if(!isset($do_not_display) || (isset($do_not_display) && $do_not_display === FALSE)){ ?>
             <em>Real Update &copy; <?php echo date("Y"); ?></em>
+            <?php } ?>
         </div>
     </body>
 </html>
