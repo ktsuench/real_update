0be0b746b477ab735469287acd0541a311e7cc34
ktsuench <will289@hotmail.com>
2016-06-27 15:56:45 -0400

    minor change made

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,5 @@
-        <div>
-        	<?php if(!isset($do_not_display) || (isset($do_not_display) && $do_not_display === FALSE)){ ?>
-            <em>Real Update &copy; <?php echo date("Y"); ?></em>
-            <?php } ?>
-        </div>
+        <?php if(!isset($do_not_display) || (isset($do_not_display) && $do_not_display === FALSE)){ ?>
+        	<div><em>Real Update &copy; <?php echo date("Y"); ?></em></div>
+        <?php } ?>
     </body>
 </html>
