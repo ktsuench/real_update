ef431199e41f8f74dac51adf94d7046434a6346e
ktsuench <will289@hotmail.com>
2016-06-27 18:57:06 -0400

    trimmed code and added in images to the announcement content sliders

--- application/views/announcements/display/index.php
+++ application/views/announcements/display/index.php
@@ -1,70 +1,63 @@
 <?php 
     echo heading($title, 2, 'id="school"'); 
 //------------------------------------------------------------------------//
+    function build_slider($content, $path){
+        foreach($content as $a){
+?>
+            <div class="flex-panel">
+                <div class="flex flex-col flex-center">
+                    <div class="content">
+                        <div class='content-title'><?php echo $a['title'] ?></div>
+                        <div class='content-material'><?php echo $a['content'] ?></div>
+                        <?php if(isset($a['image'])){ ?>
+                            <img src='<?php echo $path.$a['image']; ?>'>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+<?php
+        }
+    }
+
+    function default_slide(){
+?>
+        <div class="flex-panel">
+            <div class="flex flex-col flex-center">
+                <div class="content">
+                    <?php
+                        /*
+                         *  TODO: Add in setting to set the default 
+                         *        message when there is no content to show
+                         */
+                    ?>
+                    <div class='content-title'>No Announcements on Display</div>
+                    <div class='content-material'>There's nothing to show</div>
+                </div>
+            </div>
+        </div>
+<?php
+    }
+//------------------------------------------------------------------------//
 ?>
 <div id='display' class="flex flex-row flex-container center">
     <div id="ann-col-1" class="scroll-content-parent">
         <div class="scroll-content">
-            <?php if(!empty($announcement)){?>
-                <?php $announcement = array_reverse($announcement); ?>
-                <?php foreach($announcement as $a){ ?>
-                    <div class="flex-panel">
-                        <div class="flex flex-col flex-center">
-                            <div class="content">
-                                <h4><?php echo $a['title'] ?></h4>
-                                <div><?php echo $a['content'] ?></div>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            <?php }else{ ?>
-                <div class="flex-panel">
-                    <div class="flex flex-col flex-center">
-                        <div class="content">
-                            <?php
-                                /*
-                                 *  TODO: Add in setting to set the default 
-                                 *        message when there is no content to show
-                                 */
-                            ?>
-                            <h4>No Announcements on Display</h4>
-                            <div>There's nothing to show</div>
-                        </div>
-                    </div>
-                </div>
-            <?php } ?>
+            <?php 
+                if(!empty($announcement)){
+                    $announcement[] = array_shift($announcement);
+                    build_slider($announcement, $upload_path);
+                }else default_slide();
+            ?>
         </div>
     </div>
     <div id="ann-col-2" class="scroll-content-parent">
         <div class="scroll-content">
-            <?php if(!empty($announcement)){?>
-                <?php $announcement[] = array_shift($announcement);?>
-                <?php foreach($announcement as $a){ ?>
-                    <div class="flex-panel">
-                        <div class="flex flex-col flex-center">
-                            <div class="content">
-                                <h4><?php echo $a['title'] ?></h4>
-                                <div><?php echo $a['content'] ?></div>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            <?php }else{ ?>
-                <div class="flex-panel">
-                    <div class="flex flex-col flex-center">
-                        <div class="content">
-                            <?php
-                                /*
-                                 *  TODO: Add in setting to set the default 
-                                 *        message when there is no content to show
-                                 */
-                            ?>
-                            <h4>No Announcements on Display</h4>
-                            <div>There's nothing to show</div>
-                        </div>
-                    </div>
-                </div>
-            <?php } ?>
+            <?php 
+                if(!empty($announcement)){
+                    $announcement[] = array_shift($announcement);
+                    build_slider($announcement, $upload_path);
+                }else default_slide();
+            ?>
         </div>
     </div>
     <div id="ann-col-3" class="flex flex-col">
@@ -94,13 +87,10 @@
         </div>
     </div>
 </div>
-<!--<?php
-//------------------------------------------------------------------------//    
-    //Announcement List
-    $link_content = 'Back to Announcement List';
-    echo anchor(base_url('announcement'), $link_content, 'title="'.$link_content.'"');
-?>-->
 <link rel="stylesheet" type="text/css" href='<?php echo base_url(''); ?>assets/vendor/css/weather-icons.min.css'>
-<script>var update_display = '<?php echo base_url('announcement/display/update'); ?>';</script>
-<script>var update_weather = '<?php echo base_url('announcement/display/update/weather'); ?>';</script>
+<script>
+    var update_display = '<?php echo base_url('announcement/display/update'); ?>';
+    var update_weather = '<?php echo base_url('announcement/display/update/weather'); ?>';
+    var upload_path = '<?php echo $upload_path; ?>';
+</script>
 <script src="<?php echo base_url(''); ?>assets/js/ann_disp.js"></script>
