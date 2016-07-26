35ed47d111900e469c973e1ad8ca56e47215829f
ktsuench <will289@hotmail.com>
2016-07-25 22:50:38 -0400

    added in settings to be autoloaded

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('html', 'url');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array('');
+$autoload['config'] = array('app/settings');
 
 /*
 | -------------------------------------------------------------------
