f27a3a6d4840d40f35d37b04152f7ae00a2a1d84
ktsuench <will289@hotmail.com>
2016-10-21 14:38:20 -0400

    added in pug view library, updated methods

--- application/controllers/Navigation.php
+++ application/controllers/Navigation.php
@@ -3,50 +3,145 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require_once './application/config/app/constants.php';
 
-class Navigation extends CI_Controller{    
+class Navigation extends CI_Controller{
+    /**
+     * Instance Variables
+     */
+    protected static $_data;
+    protected static $restricted = array(
+        'announcements',
+        'dashboard',
+        'errors',
+        'includes',
+        'templates',
+        'users'
+    );
+
+    /**
+     * Constructor
+     */
     public function __construct(){
         parent::__construct();
         date_default_timezone_set('America/Toronto');
+
+        /**
+         * Default load Pug HTML Template Engine
+         */
+        $this->load->library('pug/pug', array(
+            'cache' => true,
+            'prettyprint' => true
+        ));
+
+        /**
+         * Exposing backend data to views
+         */
+        self::$_data = array(
+            'baseUrl'   =>  base_url(),
+            'env'       =>  (object) array(
+                'dev'   =>  ENVIRONMENT == ENV_DEVELOPMENT,
+                'prod'  =>  ENVIRONMENT == ENV_PRODUCTION,
+                'test'  =>  ENVIRONMENT == ENV_TESTING
+            ),
+            'user'      =>  isset($this->session->user) ? $this->session->user : NULL,
+            'year'      =>  date('Y')
+        );
     }
     
+    /**
+     * Home page
+     */
     public function index(){
+        $data = self::$_data;
         $data['title'] = 'Home';
-        
-        $this->load->view('templates/header', $data);
-        $this->load->view('index');
-        $this->load->view('templates/footer');
+        $data['userLoggedIn'] = isset($this->session->user);
+
+        /**
+         * CI view loading
+         */
+        // $this->load->view('templates/header', $data);
+        // $this->load->view('index');
+        // $this->load->view('templates/footer');
+
+        /**
+         * Pug view loading
+         */
+        // $this->load->vars($data);
+        // $this->pug->view('index');
+
+        /**
+         * Navigation class view loading
+         */
+        //self::load_view('', $data);
+        redirect('login');
     }
     
-    //Used mainly for redirection
+    /**
+     * Used mainly for redirection
+     * @param  [type]  $page [description]
+     * @param  boolean $auth [description]
+     * @return [type]        [description]
+     */
     public function load_page($page, $auth = FALSE){
+        //Check that the page being accessed is not in the restricted list
+        foreach (self::$restricted as $not_accessable) {
+            if(stripos($page, $not_accessable) !== FALSE){
+                show_404();
+            }
+        }
+
         if(isset($this->session->data)){
            $data = $this->session->data;
            unset($_SESSION['data']);
         }
-        if(!isset($data['title'])) $data['title'] = strtoupper(substr($page, 0, 1)).substr($page, 1);
+
+        if(!isset($data['title']))
+            $data['title'] = ucfirst($page);
         
-        $this->load->view('templates/header', $data);
-        if(isset($this->session->auth_check_req) || $auth){
-            if($this->session->auth_check_req || $auth){
-                $this->load->view('templates/auth_check', $data);
-                $this->load->view('templates/side_panel', $data);
-            }
+        if(isset($this->session->auth_check_req)){
+            $auth = $this->session->auth_check_req;
+            unset($_SESSION['auth_check_req']);
         }
-        $this->load->view('templates/container');
-        $this->load->view($page.'/', $data);
-        $this->load->view('templates/footer');
+
+        self::load_view($page, $data, $auth);
     }
     
-    //Used to load views from within controller
+    /**
+     * Used to load views
+     * @param  [type]  $page [description]
+     * @param  [type]  $data [description]
+     * @param  boolean $auth [description]
+     * @return [type]        [description]
+     */
     protected function load_view($page, $data = NULL, $auth = FALSE){
-        $this->load->view('templates/header', $data);
+        //$this->load->view('templates/header', $data);
+        
         if($auth || $auth === 'TRUE'){
-            $this->load->view('templates/auth_check', $data);
-            $this->load->view('templates/side_panel', $data);
+            $auth_val = isset($data['admin_access_only']) ? $data['admin_access_only'] : FALSE;
+            self::auth_check($auth_val);
+            // CI view loading
+            //$this->load->view('templates/side_panel', $data);
         }
-        $this->load->view('templates/container');
+        
+        // CI view loading
+        /*$this->load->view('templates/container');
         $this->load->view($page.'/', $data);
-        $this->load->view('templates/footer');
+        $this->load->view('templates/footer');*/
+
+        // Pug view loading
+        $this->load->vars(array_merge(self::$_data, $data));
+        $this->pug->view($page.'/index');
+    }
+
+    protected function auth_check($admin_access_only = FALSE){
+        if(!isset($this->session->authenticated) || !$this->session->authenticated) 
+            redirect('login');
+
+        if($admin_access_only && $this->session->user->type != ADMIN){
+            $this->session->access_flag = 'You do not have permission to complete the requested operation.';
+            $this->session->mark_as_flash('access_flag');
+
+            redirect('dashboard');
+        }
     }
 
     /**
@@ -187,23 +282,24 @@ class Navigation extends CI_Controller{
             //Passed in arguments
             if(!empty($extra)){
                 $extra = explode(',', $extra);
-                if(array_key_exists('0', $extra)) $field = strval($extra[0]);
+                preg_match('/^(.+)*\[/', $extra[0], $extra[0]);
+                if(array_key_exists('0', $extra)) $field = strval($extra[0][1]);
                 if(array_key_exists('1', $extra)) $name = strval($extra[1]);
                 if(array_key_exists('2', $extra)) $tmp_upload = boolval($extra[2]);
                 if(array_key_exists('3', $extra)) $dir = strval($extra[3]);
                 if(array_key_exists('4', $extra)) $sess_var = strval($extra[4]);
+            }else{
+                throw new Exception('Field name not provided.');
             }
 
-            if(empty($field)) throw new Exception('Field name not provided for '.$field.'.');
-
-            $err = array_key_exists($field, $_FILES) ? $_FILES[$field]['error'] : UPLOAD_ERR_NO_FILE;
+            $err = array_key_exists($field, $_FILES) ? $_FILES[$field]['error']['image'] : UPLOAD_ERR_NO_FILE;
             $err = self::upload_error_check($err);
 
             if($err === UPLOAD_ERR_OK){
-                if(strpos($_FILES[$field]['type'], 'image') === 0){
-                    $tmp_name = $_FILES[$field]['tmp_name'];
-                    $ext = pathinfo($_FILES[$field]['name'], PATHINFO_EXTENSION);
-                    $name = empty($name) ? basename($_FILES[$field]['name']) : $name.'.'.$ext;
+                if(strpos($_FILES[$field]['type']['image'], 'image') === 0){
+                    $tmp_name = $_FILES[$field]['tmp_name']['image'];
+                    $ext = pathinfo($_FILES[$field]['name']['image'], PATHINFO_EXTENSION);
+                    $name = empty($name) ? basename($_FILES[$field]['name']['image']) : $name.'.'.$ext;
 
                     if($tmp_upload === TRUE){
                         if(move_uploaded_file($tmp_name, './'.UPLOAD_TMP.$name)){
@@ -235,17 +331,5 @@ class Navigation extends CI_Controller{
             log_message('debug', $e->getMessage());
         }
     }
-
-    //Used to load assets from within views (scripts, stylesheets, images)
-    /*public function load_asset($asset = FALSE){
-        if(!empty($this->uri->segment(3))){
-            $i = 2; $asset = $this->uri->segment($i++);
-            do{
-                $asset .= '/' . $this->uri->segment($i++);
-            }while(!empty($this->uri->segment($i)));
-        }
-
-        $this->load->view($asset);
-    }*/
 }
 ?>
